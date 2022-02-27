367385fd2e672b13e997e3d45a31a2852ef8229d
judyblue8 <74081546+judyblue8@users.noreply.github.com>
2022-02-27 13:59:56 -0500

    Update num_BTC.py

--- num_BTC.py
+++ num_BTC.py
@@ -1,7 +1,17 @@
 import math
 
+def reward(b):
+    r = 50.0
+    h = 210000
+    if (n < h):
+        return r
+    else:
+        return reward(b-h)/2
+
 def num_BTC(b):
-    c = float(0)
-    return c
+    a = 0.0
+    for x in range (0,b):
+        a = a + reward(x)
+    return a
 
 
